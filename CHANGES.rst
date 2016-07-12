a78798223fc405222d50916f0322119573b98774
Timo Stollenwerk <tisto@plone.org>
2016-07-12 15:45:19 +0200

    Preparing release 1.0a1

--- CHANGES.rst
+++ CHANGES.rst
@@ -1,4 +1,4 @@
-1.0a1 (unreleased)
+1.0a1 (2016-07-12)
 ------------------
 
 - Initial release.
--- setup.py
+++ setup.py
@@ -1,6 +1,6 @@
 from setuptools import setup, find_packages
 
-version = '1.0.dev0'
+version = '1.0a1'
 
 long_description = (
     open('README.rst').read() +
