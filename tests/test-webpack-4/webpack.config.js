7eece173cee01eb7b7a54cca5feaef998a1a9293
Timo Stollenwerk <tisto@plone.org>
2018-03-01 22:22:55 +0100

    Add Webpack 4 test.

---
+++ tests/test-webpack-4/webpack.config.js
@@ -0,0 +1,29 @@
+const HtmlWebPackPlugin = require("html-webpack-plugin");
+
+module.exports = {
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader"
+        }
+      },
+      {
+        test: /\.html$/,
+        use: [
+          {
+            loader: "html-loader",
+            options: { minimize: true }
+          }
+        ]
+      }
+    ]
+  },
+  plugins: [
+    new HtmlWebPackPlugin({
+      filename: "./index.html"
+    })
+  ]
+};
